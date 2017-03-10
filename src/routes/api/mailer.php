a0759edac1a0e86ea0d9e6f4452d90f547f8c174
Julio Quintana <jquintana1801@gmail.com>
2017-03-10 12:09:43 -0400

    Enviando mensajes por mailgun con parametros enviados por POST

--- src/routes/api/mailer.php
+++ src/routes/api/mailer.php
@@ -4,16 +4,29 @@ use \Psr\Http\Message\ResponseInterface as Response;
 use Mailgun\Mailgun;
 
 // Obtener todos los clientes
-$app->post('/mailer/messages',function(Request $request, Response $response){
+$app->get('/mailer/messages/{accion}',function(Request $request, Response $response){
+	$accion = $request->getAttribute('accion');
 	$mgClient = new Mailgun('key-f76c8e9de1f0a5fbb71d7bd7e3bbc742');
 	$domain = "jqemprendedorve.com";
 
-	$result = $mgClient->sendMessage($domain, array(
-	    'from'    => 'Usuario emocionado <julioq_01@hotmail.com>',
-	    'to'      => 'Julio Quintana <jquintana1801@gmail.com>',
-	    'subject' => 'Hola desde API',
-	    'text'    => 'Testing some Mailgun awesomness!'
-	));
+	switch ($accion) {
+		case 'descuento':
+			$result = $mgClient->sendMessage($domain, array(
+			    'from'    => $_GET['from'],
+			    'to'      => 'Julio Quintana <jquintana1801@gmail.com>',
+			    'subject' => 'Gracias, ud ha recibido un 30% en su proximo proyecto',
+			    'text'    => 'Testing some Mailgun awesomness!'
+			));
+			break;
+		case 'contactame':
+			$result = $mgClient->sendMessage($domain, array(
+			    'from'    => $_GET['from'],
+			    'to'      => 'Julio Quintana <jquintana1801@gmail.com>',
+			    'subject' => 'Consulta para jqEmprendedorVE',
+			    'text'    => 'Testing some Mailgun awesomness!'
+			));
+			break;		
+	}
 
 	echo json_encode($result);
 });
